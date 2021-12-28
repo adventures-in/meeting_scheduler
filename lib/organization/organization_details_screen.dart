015dad04fa1764550a747379bd78bbc5e353492b
Nick Meinhold <nick@enspyr.co>
2021-12-28 16:24:52 +1100

    Add const everywhere analyzer complains

--- lib/organization/organization_details_screen.dart
+++ lib/organization/organization_details_screen.dart
@@ -5,7 +5,7 @@ class OrganizationDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('Organization details page'),
       ),
