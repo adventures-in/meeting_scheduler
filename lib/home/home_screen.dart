3012d57d61cea33988f727c0959384e0f63b6dad
Nick Meinhold <nick@enspyr.co>
2021-12-28 16:18:21 +1100

    Update to match style guide
    - removed commented code
    - changed ScreenHome to HomeScreen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class ScreenHome extends StatelessWidget {
-  const ScreenHome({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('Home page'),
       ),
